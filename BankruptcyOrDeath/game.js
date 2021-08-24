16cfb6c1f026185bb0001dbe75fa3fc593038aff
Jimmy Dutton <7128697+jdutton1439@users.noreply.github.com>
2021-08-24 10:11:08 -0500

    Update game.js

--- BankruptcyOrDeath/game.js
+++ BankruptcyOrDeath/game.js
@@ -8,7 +8,8 @@ const result = document.getElementById("result-div");
 const scenarios = 
       [
 	      "You have been diagnosed with a treatable form of cancer. Without treatment, it is terminal.",
-	      "You joined a ride-sharing service to bring in extra money when your hours were cut due to excessive staffing. During a ride, you were involved in a hit-and-run that killed your passenger and left you critically injured. When you wake in the hospital, you learn that you will likely die within five years unless you undergo a series of pricey procedures and a lifetime of physical therapy. Your ride-share company is not required to pay workers compensation, and it refuses to do so. Your car insurance will cover your visit so far, but not the following procedures or therapy."
+	      "You joined a ride-sharing service to bring in extra money when your hours were cut due to excessive staffing. During a ride, you were involved in a hit-and-run that killed your passenger and left you critically injured. When you wake in the hospital, you learn that you will likely die within five years unless you undergo a series of pricey procedures and a lifetime of physical therapy. Your ride-share company is not required to pay workers compensation, and it refuses to do so. Your car insurance will cover your visit so far, but not the following procedures or therapy.",
+	      "You exhausted your savings paying to treat your child who developed a deadly, skin-eating bacterial infection, only for the symptoms to return a month later."
       ];
 
 bankrupt.addEventListener("click", submitChoice);
